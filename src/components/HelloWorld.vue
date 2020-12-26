a6ae1cb6b7cae8ddb5c3ed8a8003eb13514c865b
Truong Van Phu <phugodness@gmail.com>
2020-12-26 10:14:53 +0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="mt-5 p-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="../assets/profile.jpg" alt="Woman's Face">
+    <div class="text-center space-y-2 sm:text-left">
+      <div class="space-y-0.5">
+        <p class="text-lg text-black font-semibold">
+          Truong Van Phu
+        </p>
+        <p class="text-gray-500 font-medium">
+          Software Engineer
+        </p>
+      </div>
+      <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+    </div>
+  </div>
+
+  <div class="text-center pt-10">
+    <!-- Using utilities -->
+    <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+      Say Hi
+    </button>
+
+    <!-- Extracting classes using @apply -->
+    <button class="btn btn-green ml-5">
+      Nice to meet you
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+  .btn {
+    @apply py-2 px-4 font-semibold rounded-lg shadow-md;
+  }
+  .btn-green {
+    @apply text-white bg-green-500 hover:bg-green-700;
+  }
+</style>
